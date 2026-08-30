96731986b55ed0bf018c0774544a25b83734f10c
grogu <giantgrogu@gmail.com>
2026-08-30 17:30:20 +0800

    Add update procedure to fix non-updating slots script

--- Assets/scripts/Inventory/Slots.cs
+++ Assets/scripts/Inventory/Slots.cs
@@ -18,6 +18,12 @@ public class Slots : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         iconImage = transform.GetChild(0).GetComponent<Image>();
         qtyText = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        
+    }
+
+    private void Update()
+    {
+        UpdateSlot();
     }
 
     public WeaponData GetItem()
