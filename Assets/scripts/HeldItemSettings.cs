1c0e5ac8db74b8372d0ebd06fd7df287133582c3
grogu <giantgrogu@gmail.com>
2026-09-10 12:02:07 +0800

    Add rotation

--- Assets/scripts/HeldItemSettings.cs
+++ Assets/scripts/HeldItemSettings.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class HeldItemSettings : MonoBehaviour
 {
+    [SerializeField] private Vector3 itemRotation;
     private void Awake()
     {
-        
+        transform.rotation = Quaternion.Euler(itemRotation);
     }
 }
