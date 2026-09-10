1d0b459e94b4a804b25fefc9363402f7e1302488
grogu <giantgrogu@gmail.com>
2026-09-10 12:18:17 +0800

    Move camera and movement script to a general file

---
+++ Assets/scripts/Movement/CameraControl.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraControl : MonoBehaviour
+{
+    public static CameraControl Instance;
+    [Header("Sensitivity")]
+    public float sensX;
+    public float sensY;
+    
+    [Header("Transform")]
+    public Transform orientation;
+    public Transform modelRotation;
+
+    public bool updateRotation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Awake()
+    {
+        Instance = this;
+        updateRotation = true;
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (!updateRotation) return;
+        Vector2 mouseDel = Mouse.current.delta.ReadValue();
+
+        xRotation -= mouseDel.y * sensX;
+        yRotation += mouseDel.x * sensY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        modelRotation.rotation = orientation.rotation;
+    }
+}
---
+++ Assets/scripts/Movement/MoveCharacter.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class MoveCharacter : MonoBehaviour
+{
+    public InputActionAsset ActionInput;
+    private InputAction jumpAction;
+    private InputAction moveAction;
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 2f;
+
+    public Rigidbody rb;
+    private Vector3 moveAmt;
+    private bool isGrounded;
+
+    private void OnEnable()
+    {
+        ActionInput.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        ActionInput.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        moveAction = InputSystem.actions.FindAction("Move");
+
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+    }
+
+    private void Update()
+    {
+        moveAmt = moveAction.ReadValue<Vector2>();
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
+        {
+            Jump();
+        }
+    }
+    // procedures for movement and general mechanics
+    
+    private void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + (transform.forward * moveAmt.y + transform.right * moveAmt.x) * Time.deltaTime * moveSpeed);
+        
+    }
+
+    private void Jump()
+    {
+        rb.AddForceAtPosition(new Vector3(0, jumpSpeed, 0), Vector3.up, ForceMode.Impulse);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if(contact.normal.y > 0.5f)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+}
