b8714a791da14a7abbeb81b1202591dd9e538f91
grogu <giantgrogu@gmail.com>
2026-09-12 14:31:16 +0800

    Add items folder and create enemy SO

---
+++ Assets/items/Enemy/EnemySO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Objects/Enemy")]
+public class EnemySO : ScriptableObject
+{
+    public int opponentHealth;
+
+    private void Awake()
+    {
+        
+    }
+
+}
