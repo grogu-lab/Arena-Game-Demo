16b9c4f004ed15207acc19c3294a55f0aa26f776
grogu <giantgrogu@gmail.com>
2026-09-12 19:55:23 +0800

    Add health + prefab gameobject field

--- Assets/items/Enemy/EnemySO.cs
+++ Assets/items/Enemy/EnemySO.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Objects/Enemy")]
 public class EnemySO : ScriptableObject
 {
-    public int enemyHealth;
-    private GameObject enemyPrefab;
+    public int enemyHealth = 50;
+    public GameObject enemyPrefab;
 
 }
