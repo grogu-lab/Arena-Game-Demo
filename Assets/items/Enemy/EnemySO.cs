70df310b6d3c7d75b178b19615c90c331e3cc879
grogu <giantgrogu@gmail.com>
2026-09-12 14:38:52 +0800

    add health-bar and prefab fields for enemy SOs

--- Assets/items/Enemy/EnemySO.cs
+++ Assets/items/Enemy/EnemySO.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Objects/Enemy")]
 public class EnemySO : ScriptableObject
 {
-    public int opponentHealth;
-
-    private void Awake()
-    {
-        
-    }
+    public int enemyHealth;
+    private GameObject enemyPrefab;
 
 }
