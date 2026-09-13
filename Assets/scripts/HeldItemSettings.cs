953f6ed2bc62f63d639ec99e053a44aaf2bd0d06
grogu <giantgrogu@gmail.com>
2026-09-13 19:13:50 +0800

    Add input action field for attack options

--- Assets/scripts/HeldItemSettings.cs
+++ Assets/scripts/HeldItemSettings.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HeldItemSettings : MonoBehaviour
 {
     public static HeldItemSettings HeldItemInstance;
+
+    public InputActionAsset WeaponInputs;
+    private InputAction attack;
+
     [SerializeField] public Vector3 itemRotation;
     [SerializeField] public Vector3 itemHoldPosition;
 
-    [SerializeField] public int damage;
+    [SerializeField] public int damage = 10;
+
+    private void OnEnable()
+    {
+        WeaponInputs.FindActionMap("Player").Enable();
+    }
+
+    private void OnDisable()
+    {
+        WeaponInputs.FindActionMap("Player").Disable();
+    }
 
     private void Awake()
     {
         HeldItemInstance = this;
+        attack = InputSystem.actions.FindAction("Attack");
+    }
+
+    private void SwingWeapon()
+    {
+        
     }
 }
