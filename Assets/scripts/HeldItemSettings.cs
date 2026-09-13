8ee6704b7673c3a8589e38f75a163b0de549bde6
grogu <giantgrogu@gmail.com>
2026-09-13 20:32:12 +0800

    Add trigger to swing weapon for future animations

--- Assets/scripts/HeldItemSettings.cs
+++ Assets/scripts/HeldItemSettings.cs
@@ -12,24 +12,28 @@ public class HeldItemSettings : MonoBehaviour
     [SerializeField] public Vector3 itemHoldPosition;
 
     [SerializeField] public int damage = 10;
+    public bool attackPressed;
 
     private void OnEnable()
     {
         WeaponInputs.FindActionMap("Player").Enable();
+        attack.performed += SwingWeapon;
     }
 
     private void OnDisable()
     {
         WeaponInputs.FindActionMap("Player").Disable();
+        attack.performed -= SwingWeapon;
     }
 
     private void Awake()
     {
         HeldItemInstance = this;
+        attackPressed = false;
         attack = InputSystem.actions.FindAction("Attack");
     }
 
-    private void SwingWeapon()
+    private void SwingWeapon(InputAction.CallbackContext context)
     {
         
     }
