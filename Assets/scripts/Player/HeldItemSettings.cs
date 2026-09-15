ef89196ef49b03583ee8084be605ce12787b5804
grogu <giantgrogu@gmail.com>
2026-09-15 10:29:23 +0800

    Move Input action fields to MoveCharacter.cs

--- Assets/scripts/Player/HeldItemSettings.cs
+++ Assets/scripts/Player/HeldItemSettings.cs
@@ -5,20 +5,13 @@ public class HeldItemSettings : MonoBehaviour
 {
     public static HeldItemSettings HeldItemInstance;
 
-    public InputActionAsset WeaponInputs;
-    private InputAction attack;
-
     [SerializeField] public Vector3 itemRotation;
     [SerializeField] public Vector3 itemHoldPosition;
-
     [SerializeField] public int damage = 10;
-    public bool attackPressed;
 
     private void Awake()
     {
         HeldItemInstance = this;
-        attackPressed = false;
-        attack = InputSystem.actions.FindAction("Attack");
     }
 
 }
