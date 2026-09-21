d3d5568fe7e3295a7fcce13e25c57ddcebaa6f4e
grogu <giantgrogu@gmail.com>
2026-09-21 14:38:25 +0800

    Initialize throw action

--- Assets/scripts/Player/Movement/Animations/TriggerScript.cs
+++ Assets/scripts/Player/Movement/Animations/TriggerScript.cs
@@ -1,27 +1,32 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 public class TriggerScript : MonoBehaviour
 {   
     public InputActionAsset InputActions_TriggerScript;
     private InputAction attackAction;
+    private InputAction throwAction;
     private Animator swingAnimator;
 
 
     private void OnEnable()
     {
         InputActions_TriggerScript.FindActionMap("Player").Enable();
-        attackAction.performed += PerformAttack;
+        attackAction.performed += SwingAttack;
+        throwAction.performed += ThrowAttack;
     }
 
     private void OnDisable()
     {
         InputActions_TriggerScript.FindActionMap("Player").Disable();
-        attackAction.performed -= PerformAttack;
+        attackAction.performed -= SwingAttack;
+        throwAction.performed -= ThrowAttack;
     }
 
     private void Awake()
     {
         attackAction = InputSystem.actions.FindAction("Attack");
+        throwAction = InputSystem.actions.FindAction("Throw");
     }
 
     private void Start()
@@ -30,7 +35,7 @@ public class TriggerScript : MonoBehaviour
         
     }
 
-    private void PerformAttack(InputAction.CallbackContext context)
+    private void SwingAttack(InputAction.CallbackContext context)
     {
         if(HeldItemSettings.HeldItemInstance == null) return;
         if (HeldItemSettings.HeldItemInstance.gameObject.CompareTag("CloseRange") || HeldItemSettings.HeldItemInstance.gameObject.CompareTag("AllRange"))
@@ -46,6 +51,7 @@ public class TriggerScript : MonoBehaviour
     private void ThrowAttack(InputAction.CallbackContext context)
     {
         if(HeldItemSettings.HeldItemInstance == null || HeldItemSettings.HeldItemInstance.gameObject.CompareTag("CloseRange")) return;
+        
 
     }
 
