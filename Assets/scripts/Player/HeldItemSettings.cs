9453b700698a427e2bada1934d124b1826cba89e
grogu <giantgrogu@gmail.com>
2026-09-22 11:40:20 +0800

    Instantiate a throw-able object upon trigger

--- Assets/scripts/Player/HeldItemSettings.cs
+++ Assets/scripts/Player/HeldItemSettings.cs
@@ -4,14 +4,38 @@ using UnityEngine.InputSystem;
 public class HeldItemSettings : MonoBehaviour
 {
     public static HeldItemSettings HeldItemInstance;
+    public InputActionAsset actionMap;
+    public GameObject heldItemPrefab;
+    private InputAction throwAction;
 
     [SerializeField] public Vector3 itemRotation;
     [SerializeField] public Vector3 itemHoldPosition;
     [SerializeField] public int damage = 10;
 
+
+    private void OnEnable()
+    {
+        actionMap.FindActionMap("Player").Enable();
+        throwAction.performed += ThrowWeaponItem;
+    }
+    
+    private void OnDisable()
+    {
+        actionMap.FindActionMap("Player").Disable();
+        throwAction.performed -= ThrowWeaponItem;
+    }
+
     private void Awake()
     {
         HeldItemInstance = this;
+        throwAction = InputSystem.actions.FindAction("Throw");
+    }
+
+    private void ThrowWeaponItem(InputAction.CallbackContext context)
+    {
+        if(!gameObject.CompareTag("AllRange") || !gameObject.CompareTag("LongRange")) return;
+        GameObject heldItem = Instantiate(heldItemPrefab, Inventory.InstantiateInventory.hand);
+        
     }
 
 }
