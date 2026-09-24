e3eba3a66acb863dec59ee9e6c33b08315fff9aa
grogu <giantgrogu@gmail.com>
2026-09-24 13:52:25 +0800

    Add per-weapon position and rotation fields

--- Assets/scripts/Player/HeldItemSettings.cs
+++ Assets/scripts/Player/HeldItemSettings.cs
@@ -3,11 +3,16 @@ using UnityEngine.InputSystem;
 
 public class HeldItemSettings : MonoBehaviour
 {
+    public static HeldItemSettings HeldItemInstance;
     public WeaponData handWeaponItem;
     public int damageDealt;
+    public Vector3 itemHoldPosition;
+    public Vector3 itemRotation;
+
 
     private void Awake()
     {
+        HeldItemInstance = this;
         handWeaponItem = Inventory.InstantiateInventory.equipItem;
         damageDealt = handWeaponItem.dealsDamage;
     }
