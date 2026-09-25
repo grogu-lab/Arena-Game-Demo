99ce0860206a7f97eb131c4fc8c1e89c35892029
grogu <giantgrogu@gmail.com>
2026-09-25 18:31:10 +0800

    fix inconsistent right/left movement speeds

--- Assets/scripts/Player/Movement/MoveCharacter.cs
+++ Assets/scripts/Player/Movement/MoveCharacter.cs
@@ -12,9 +12,6 @@ public class MoveCharacter : MonoBehaviour
     [SerializeField] private float jumpSpeed = 2f;
 
     public Rigidbody rb;
-    public GameObject arenaObject;
-    private Vector3 arenaRange;
-    private Vector3 arenaCenter;
     private Vector3 moveAmt;
     public bool isGrounded;
 
@@ -41,17 +38,18 @@ public class MoveCharacter : MonoBehaviour
 
     private void Update()
     {
-        moveAmt = moveAction.ReadValue<Vector2>();
+        moveAmt = moveAction.ReadValue<Vector2>().normalized;
     }
     // procedures for movement and general mechanics
     
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveAmt.y * transform.forward + moveAmt.x * moveSpeed * Time.deltaTime * transform.right);
+        rb.MovePosition(rb.position + ((moveAmt.y * transform.forward) + (moveAmt.x * transform.right)) * moveSpeed * Time.deltaTime);
     }
 
     private void Jump(InputAction.CallbackContext context)
     {
+        if(!isGrounded) return;
         rb.AddForceAtPosition(new Vector3(0, jumpSpeed, 0), Vector3.up, ForceMode.Impulse);
     }
 
